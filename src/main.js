918e863ea69c8089ed85eea5a54e1af72f2b0e5b
Ahmad Alfarizki <54702541+AhmdFarzki@users.noreply.github.com>
2020-12-29 16:18:39 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap";
 
 import axios from "axios";
-axios.defaults.baseURL = "http://127.0.0.1:8000/";
+axios.defaults.baseURL = "https://vuenote.madfariz.web.id/public_html/";
 
 Vue.config.productionTip = false;
 
